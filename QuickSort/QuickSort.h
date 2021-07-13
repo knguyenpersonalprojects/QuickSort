3f13bc813435bb233b38520500ecefa2b4e1790c
nguyenkSon2398 <44067551+nguyenkSon2398@users.noreply.github.com>
2021-07-13 17:28:46 -0500

    Add project files.

---
+++ QuickSort/QuickSort.h
@@ -0,0 +1,8 @@
+#pragma once
+
+void Swapping(int* a, int* b);
+//void QuickSort(int arr[], int low, int high);
+void QuickSortMid(int arr[], int low, int high, int size);
+//int PartitionRight(int arr[],int low, int high);
+int PartitionMid(int arr[], int low, int high, int size);
+void PrintArr(int arr[],int size);
---
+++ QuickSort/Source.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include"QuickSort.h"
+using namespace std;
+
+int main()
+{
+	int arr[] = { 10, 7, 8, 9, 1, 5 };
+	int size = sizeof(arr) / sizeof(arr[0]);
+	QuickSortMid(arr, 0, size-1,size);
+	PrintArr(arr, size);
+}
